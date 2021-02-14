1a1ce6bfcfbbda681297733022531d1469285f8b
lingzhan <m18340872285@163.com>
2021-02-14 21:52:51 +0800

    feat: ExecutionContext,注册TableSource和TableSink新方式

--- src/main/java/com/flink/platform/core/context/ExecutionContext.java
+++ src/main/java/com/flink/platform/core/context/ExecutionContext.java
@@ -1,7 +1,8 @@
 package com.flink.platform.core.context;
 
+import cn.hutool.core.lang.func.Func;
 import com.flink.platform.core.config.Environment;
-import com.flink.platform.core.config.entries.DeploymentEntry;
+import com.flink.platform.core.config.entries.*;
 import com.flink.platform.core.exception.SqlExecutionException;
 import org.apache.commons.cli.CommandLine;
 import org.apache.commons.cli.Options;
@@ -16,28 +17,36 @@ import org.apache.flink.client.deployment.ClusterClientServiceLoader;
 import org.apache.flink.configuration.Configuration;
 import org.apache.flink.streaming.api.TimeCharacteristic;
 import org.apache.flink.streaming.api.environment.StreamExecutionEnvironment;
-import org.apache.flink.table.api.EnvironmentSettings;
-import org.apache.flink.table.api.TableConfig;
-import org.apache.flink.table.api.TableEnvironment;
-import org.apache.flink.table.api.TableException;
+import org.apache.flink.table.api.*;
+import org.apache.flink.table.api.bridge.java.BatchTableEnvironment;
+import org.apache.flink.table.api.bridge.java.StreamTableEnvironment;
+import org.apache.flink.table.api.bridge.java.internal.BatchTableEnvironmentImpl;
+import org.apache.flink.table.api.bridge.java.internal.StreamTableEnvironmentImpl;
+import org.apache.flink.table.api.internal.TableEnvironmentInternal;
+import org.apache.flink.table.catalog.Catalog;
 import org.apache.flink.table.catalog.CatalogManager;
 import org.apache.flink.table.catalog.FunctionCatalog;
 import org.apache.flink.table.catalog.GenericInMemoryCatalog;
 import org.apache.flink.table.delegation.Executor;
 import org.apache.flink.table.delegation.ExecutorFactory;
-import org.apache.flink.table.factories.ComponentFactory;
-import org.apache.flink.table.factories.ComponentFactoryService;
+import org.apache.flink.table.delegation.Planner;
+import org.apache.flink.table.delegation.PlannerFactory;
+import org.apache.flink.table.descriptors.CoreModuleDescriptorValidator;
+import org.apache.flink.table.factories.*;
+import org.apache.flink.table.functions.*;
+import org.apache.flink.table.module.Module;
 import org.apache.flink.table.module.ModuleManager;
+import org.apache.flink.table.sinks.TableSink;
+import org.apache.flink.table.sources.TableSource;
 import org.apache.flink.util.FlinkException;
+import org.apache.flink.util.TemporaryClassLoaderContext;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
 import javax.annotation.Nullable;
 import java.lang.reflect.Method;
 import java.net.URL;
-import java.util.Collections;
-import java.util.List;
-import java.util.Map;
+import java.util.*;
 import java.util.stream.Collectors;
 import java.util.stream.Stream;
 
@@ -99,7 +108,12 @@ public class ExecutionContext<ClusterID> {
                 environment.getDeployment(),
                 commandLineOptions);
 
-
+        // 将命令行参数添加到Flink配置中
+        flinkConfig.addAll(createExecutionConfig(commandLine,
+                commandLineOptions,
+                availableCommandLines,
+                dependencies)
+        );
 
 
         // ClusterClientFactory初始化
@@ -207,9 +221,245 @@ public class ExecutionContext<ClusterID> {
             // Step 1.3 Set up session state.
             this.sessionState = SessionState.of(catalogManager, moduleManager, functionCatalog);
 
+            // Must initialize the table environment before actually the
+            createTableEnvironment(settings, config, catalogManager, moduleManager, functionCatalog);
+
+            //--------------------------------------------------------------------------------------------------------------
+            // Step.2 Create modules and load them into the TableEnvironment.
+            //--------------------------------------------------------------------------------------------------------------
+            // No need to register the modules info if already inherit from the same session.
+            Map<String, Module> modules = new LinkedHashMap<>();
+            environment.getModules().forEach((name, entry) ->
+                    modules.put(name, createModule(entry.asMap(), classLoader))
+            );
+            if (!modules.isEmpty()) {
+                // unload core module first to respect whatever users configure
+                tableEnv.unloadModule(CoreModuleDescriptorValidator.MODULE_TYPE_CORE);
+                modules.forEach(tableEnv::loadModule);
+            }
+
+            //--------------------------------------------------------------------------------------------------------------
+            // Step.3 create user-defined functions and temporal tables then register them.
+            //--------------------------------------------------------------------------------------------------------------
+            // No need to register the functions if already inherit from the same session.
+            registerFunctions();
+
+            //--------------------------------------------------------------------------------------------------------------
+            // Step.4 Create catalogs and register them.
+            //--------------------------------------------------------------------------------------------------------------
+            // No need to register the catalogs if already inherit from the same session.
+            initializeCatalogs();
 
         }
 
+    }
+
+    /**
+     * Executes the given Runnable using the execution context's classloader as thread classloader.
+     */
+    void wrapClassLoader(Runnable runnable) {
+        try (TemporaryClassLoaderContext ignored = TemporaryClassLoaderContext.of(classLoader)) {
+            runnable.run();
+        }
+    }
+
+    private Catalog createCatalog(String name, Map<String, String> catalogProperties, ClassLoader classLoader) {
+        final CatalogFactory factory =
+                TableFactoryService.find(CatalogFactory.class, catalogProperties, classLoader);
+        return factory.createCatalog(name, catalogProperties);
+    }
+
+    private static TableSource<?> createTableSource(ExecutionEntry execution, Map<String, String> sourceProperties,
+                                                    ClassLoader classLoader) {
+        if (execution.isStreamingPlanner()) {
+            final TableSourceFactory<?> factory = (TableSourceFactory<?>)
+                    TableFactoryService.find(TableSourceFactory.class, sourceProperties, classLoader);
+            return factory.createTableSource(sourceProperties);
+        } else if (execution.isBatchPlanner()) {
+            final BatchTableSourceFactory<?> factory = (BatchTableSourceFactory<?>)
+                    TableFactoryService.find(BatchTableSourceFactory.class, sourceProperties, classLoader);
+            return factory.createBatchTableSource(sourceProperties);
+        }
+        throw new SqlExecutionException("Unsupported execution type for sources.");
+    }
+
+    private static TableSink<?> createTableSink(ExecutionEntry execution, Map<String, String> sinkProperties,
+                                                ClassLoader classLoader) {
+        if (execution.isStreamingPlanner()) {
+            final TableSinkFactory<?> factory = (TableSinkFactory<?>)
+                    TableFactoryService.find(TableSinkFactory.class, sinkProperties, classLoader);
+            return factory.createTableSink(sinkProperties);
+        } else if (execution.isBatchPlanner()) {
+            final BatchTableSinkFactory<?> factory = (BatchTableSinkFactory<?>)
+                    TableFactoryService.find(BatchTableSinkFactory.class, sinkProperties, classLoader);
+            return factory.createBatchTableSink(sinkProperties);
+        }
+        throw new SqlExecutionException("Unsupported execution type for sinks.");
+    }
+
+
+
+    private void initializeCatalogs() {
+        //--------------------------------------------------------------------------------------------------------------
+        // Step.1 Create catalogs and register them.
+        //--------------------------------------------------------------------------------------------------------------
+        wrapClassLoader(() -> {
+            environment.getCatalogs().forEach((name, entry) -> {
+                Catalog catalog = createCatalog(name, entry.asMap(), classLoader);
+                tableEnv.registerCatalog(name, catalog);
+            });
+        });
+
+        //--------------------------------------------------------------------------------------------------------------
+        // Step.2 create table sources & sinks, and register them.
+        //--------------------------------------------------------------------------------------------------------------
+        Map<String, TableSource<?>> tableSources = new HashMap<>();
+        Map<String, TableSink<?>> tableSinks = new HashMap<>();
+        environment.getTables().forEach((name, entry) -> {
+            if (entry instanceof SourceTableEntry || entry instanceof SourceSinkTableEntry) {
+                tableSources.put(name, createTableSource(environment.getExecution(), entry.asMap(), classLoader));
+            }
+            if (entry instanceof SinkTableEntry || entry instanceof SourceSinkTableEntry) {
+                tableSinks.put(name, createTableSink(environment.getExecution(), entry.asMap(), classLoader));
+            }
+        });
+        // register table sources 已废弃
+        // tableSources.forEach(tableEnv::registerTableSource);
+        // register table sinks 已废弃
+        // tableSinks.forEach(tableEnv::registerTableSink);
+
+        tableSources.forEach(((TableEnvironmentInternal) tableEnv)::registerTableSourceInternal);
+        // register table sinks
+        tableSinks.forEach(((TableEnvironmentInternal) tableEnv)::registerTableSinkInternal);
+
+        //--------------------------------------------------------------------------------------------------------------
+        // Step.4 Register temporal tables.
+        //--------------------------------------------------------------------------------------------------------------
+        environment.getTables().forEach((name, entry) -> {
+            if (entry instanceof TemporalTableEntry) {
+                final TemporalTableEntry temporalTableEntry = (TemporalTableEntry) entry;
+                registerTemporalTable(temporalTableEntry);
+            }
+        });
+
+        //--------------------------------------------------------------------------------------------------------------
+        // Step.5 Register views in specified order.
+        //--------------------------------------------------------------------------------------------------------------
+        environment.getTables().forEach((name, entry) -> {
+            // if registering a view fails at this point,
+            // it means that it accesses tables that are not available anymore
+            if (entry instanceof ViewEntry) {
+                final ViewEntry viewEntry = (ViewEntry) entry;
+                registerView(viewEntry);
+            }
+        });
+
+        //--------------------------------------------------------------------------------------------------------------
+        // Step.6 Set current catalog and database.
+        //--------------------------------------------------------------------------------------------------------------
+        // Switch to the current catalog.
+        Optional<String> catalog = environment.getExecution().getCurrentCatalog();
+        catalog.ifPresent(tableEnv::useCatalog);
+
+        // Switch to the current database.
+        Optional<String> database = environment.getExecution().getCurrentDatabase();
+        database.ifPresent(tableEnv::useDatabase);
+    }
+
+    /**
+     * 注册视图
+     * @param viewEntry
+     */
+    private void registerView(ViewEntry viewEntry) {
+        try {
+            tableEnv.registerTable(viewEntry.getName(), tableEnv.sqlQuery(viewEntry.getQuery()));
+        } catch (Exception e) {
+            throw new SqlExecutionException(
+                    "Invalid view '" + viewEntry.getName() + "' with query:\n" + viewEntry.getQuery()
+                            + "\nCause: " + e.getMessage());
+        }
+    }
+
+    /**
+     * 注册临时表
+     * @param temporalTableEntry
+     */
+    private void registerTemporalTable(TemporalTableEntry temporalTableEntry) {
+        try {
+            final Table table = tableEnv.scan(temporalTableEntry.getHistoryTable());
+            final TableFunction<?> function = table.createTemporalTableFunction(
+                    temporalTableEntry.getTimeAttribute(),
+                    String.join(",", temporalTableEntry.getPrimaryKeyFields()));
+            if (tableEnv instanceof StreamTableEnvironment) {
+                StreamTableEnvironment streamTableEnvironment = (StreamTableEnvironment) tableEnv;
+                streamTableEnvironment.registerFunction(temporalTableEntry.getName(), function);
+            } else {
+                BatchTableEnvironment batchTableEnvironment = (BatchTableEnvironment) tableEnv;
+                batchTableEnvironment.registerFunction(temporalTableEntry.getName(), function);
+            }
+        } catch (Exception e) {
+            throw new SqlExecutionException(
+                    "Invalid temporal table '" + temporalTableEntry.getName() + "' over table '" +
+                            temporalTableEntry.getHistoryTable() + ".\nCause: " + e.getMessage());
+        }
+    }
+
+
+    /**
+     * 注册自定义方法
+     */
+    private void registerFunctions(){
+        // 顺序注册
+        Map<String, FunctionDefinition> functions=new LinkedHashMap<>();
+        environment.getFunctions().forEach((name,entry) -> {
+            final UserDefinedFunction function = FunctionService.createFunction(
+                    entry.getDescriptor(),classLoader,false
+            );
+            functions.put(name,function);
+        });
+        // 注册自定义方法
+        registerFunctions(functions);
+    }
+
+    private void registerFunctions(Map<String, FunctionDefinition> functions) {
+        if (tableEnv instanceof StreamTableEnvironment) {
+            StreamTableEnvironment streamTableEnvironment = (StreamTableEnvironment) tableEnv;
+            functions.forEach((k, v) -> {
+                if (v instanceof ScalarFunction) {
+                    streamTableEnvironment.registerFunction(k, (ScalarFunction) v);
+                } else if (v instanceof AggregateFunction) {
+                    streamTableEnvironment.registerFunction(k, (AggregateFunction<?, ?>) v);
+                } else if (v instanceof TableFunction) {
+                    streamTableEnvironment.registerFunction(k, (TableFunction<?>) v);
+                } else {
+                    throw new SqlExecutionException("Unsupported function type: " + v.getClass().getName());
+                }
+            });
+        } else {
+            BatchTableEnvironment batchTableEnvironment = (BatchTableEnvironment) tableEnv;
+            functions.forEach((k, v) -> {
+                if (v instanceof ScalarFunction) {
+                    batchTableEnvironment.registerFunction(k, (ScalarFunction) v);
+                } else if (v instanceof AggregateFunction) {
+                    batchTableEnvironment.registerFunction(k, (AggregateFunction<?, ?>) v);
+                } else if (v instanceof TableFunction) {
+                    batchTableEnvironment.registerFunction(k, (TableFunction<?>) v);
+                } else {
+                    throw new SqlExecutionException("Unsupported function type: " + v.getClass().getName());
+                }
+            });
+        }
+    }
+
+    /**
+     * 创建module
+     * @param moduleProperties module配置属性
+     * @param classLoader 类加载器
+     */
+    private Module createModule(Map<String, String> moduleProperties, ClassLoader classLoader) {
+        final ModuleFactory factory =
+                TableFactoryService.find(ModuleFactory.class, moduleProperties, classLoader);
+        return factory.createModule(moduleProperties);
     }
 
     /**
@@ -233,12 +483,64 @@ public class ExecutionContext<ClusterID> {
             execEnv = null;
 
             final Map<String,String> executorProperties=settings.toExecutorProperties();
+            executor=lookupExecutor(executorProperties,streamExecEnv);
+            // 初始化完流处理环境后,再初始化StreamTableEnvironment
+            tableEnv=createStreamTableEnvironment(
+                    streamExecEnv,
+                    settings,
+                    config,
+                    executor,
+                    catalogManager,
+                    moduleManager,
+                    functionCatalog
+            );
+        }else if (environment.getExecution().isBatchPlanner()) {
+            streamExecEnv = null;
+            execEnv = createExecutionEnvironment();
 
-
-
-
+            executor = null;
+            tableEnv = new BatchTableEnvironmentImpl(
+                    execEnv,
+                    config,
+                    catalogManager,
+                    moduleManager);
+        }else{
+            throw new SqlExecutionException("Unsupported execution type specified.");
         }
     }
+
+
+
+
+    private TableEnvironment createStreamTableEnvironment(
+            StreamExecutionEnvironment env,
+            EnvironmentSettings settings,
+            TableConfig config,
+            Executor executor,
+            CatalogManager catalogManager,
+            ModuleManager moduleManager,
+            FunctionCatalog functionCatalog){
+        // PlannerProperties ???
+        final Map<String, String> plannerProperties = settings.toPlannerProperties();
+
+        final Planner planner = ComponentFactoryService.find(PlannerFactory.class,plannerProperties)
+                .create(plannerProperties,executor,config,functionCatalog,catalogManager);
+
+        return new StreamTableEnvironmentImpl(
+                catalogManager,
+                moduleManager,
+                functionCatalog,
+                config,
+                env,
+                planner,
+                executor,
+                settings.isStreamingMode(),
+                classLoader
+        );
+    }
+
+
+
 
 
     /**
